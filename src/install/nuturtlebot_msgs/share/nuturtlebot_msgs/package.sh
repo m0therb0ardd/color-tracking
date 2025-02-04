89f86571c1791b538bdac1f587efea24c0470421
m0therb0ardd <maglionecatherine@gmail.com>
2025-02-04 13:42:07 -0600

    waypoints transformed to turtlebot frame but turtlebot detection is very jumpy

--- src/install/nuturtlebot_msgs/share/nuturtlebot_msgs/package.sh
+++ src/install/nuturtlebot_msgs/share/nuturtlebot_msgs/package.sh
@@ -78,6 +78,9 @@ _colcon_package_sh_source_script() {
 }
 
 # source sh hooks
+_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/nuturtlebot_msgs/hook/cmake_prefix_path.sh"
+_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/nuturtlebot_msgs/hook/ld_library_path_lib.sh"
+_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/nuturtlebot_msgs/hook/pythonpath.sh"
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/nuturtlebot_msgs/local_setup.sh"
 
 unset _colcon_package_sh_source_script
